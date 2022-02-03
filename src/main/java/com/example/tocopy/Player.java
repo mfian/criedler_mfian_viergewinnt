a2d5378f78d59097c682bf1ff7e7fe447be531f3
Conschtatin <75018209+Conschtatin@users.noreply.github.com>
2022-02-03 10:28:23 +0100

    Hinzufügen der Methodenkommentare und weiterprogrammierung der Funktionen

--- src/main/java/com/example/tocopy/Player.java
+++ src/main/java/com/example/tocopy/Player.java
@@ -4,18 +4,34 @@ public class Player {
     String pname;
     char symbol;
 
+    /**
+     * Gibt den namen von einem Player objekt zurück
+     * @return  den Namen
+     */
     public String getPname() {
         return pname;
     }
 
+    /**
+     * Setzt den namen von einem Player objekt zurück
+     * @param pname der Name der gesetzt werden soll
+     */
     public void setPname(String pname) {
         this.pname = pname;
     }
 
+    /**
+     * Gibt das Symbol von einem Player objekt zurück
+     * @return das Symbol
+     */
     public char getSymbol() {
         return symbol;
     }
 
+    /**
+     * Setzt das Symbol eines Player objekts
+     * @param symbol welches symbol gesetzt werden soll
+     */
     public void setSymbol(char symbol) {
         this.symbol = symbol;
     }
--- src/main/java/com/example/tocopy/Playfield.java
+++ src/main/java/com/example/tocopy/Playfield.java
@@ -1,18 +1,81 @@
 package com.example.tocopy;
 
+import java.util.Scanner;
+
 public class Playfield {
-    protected int [] [] Field = new int [6][7];
+    protected char[][] Field = new char[6][7];
 
-    Playfield(){
+    /**
+     * Konstruktor, setzt alle Felder des arrays auf "nicht belegt"
+     */
+    Playfield() {
+        for (int i = 0; i < Field.length; i++) {
 
+            for (int j = 0; j < Field[i].length; j++) {
+                Field[i][j] = '#';
+            }
+        }
     }
 
-    boolean checkdraw(){
-        boolean rv= true;
+    /**
+     * Überprüft ob das Feld zur gänze voll ist und noch kein Spieler gewonnen hat
+     * @return Ob das Spiel unentschieden ausgegangen ist oder nicht
+     */
+    boolean checkdraw() {
+        boolean rv = false;
 
         return rv;
     }
 
+    /**
+     * Fragt und lest die gewünschte Spalte ein, in der ein Stein geworfen werden soll.
+     * Es wird geprüft ob die Spalte belegt ist, wenn nicht wird ein Stein gesetzt
+     * @param currentp Übergibt den aktuellen Spieler, um zu wissen mit welchen Symbol setstone() aufgerufen werden soll
+     * @return ob die Spalte belegt war oder nicht
+     */
+    boolean choosecol(Player currentp) {
+        boolean rv = true;
+        int col;
+        System.out.println("In Welche Spalte möchten Sie einen Stein werfen?");
+        Scanner scan = new Scanner(System.in);
+        col = scan.nextInt();
+        if (!checkiffull(col)) {
+            setStone(col, currentp);
+        } else {
+            rv = false;
+        }
+        return rv;
+
+    }
+
+    /**
+     *  * Lest dass array von unten ausgehend aus und überprüft in jeder Zeile,
+     *  ob es sich um ein Freies Feld handelt. Wenn ja wird ein Stein gesetzt.
+     * @param col In welche Spalte ein Stein fallen gelassen werden soll
+     * @param currentp Welches Symbol von welchen Spieler gesetzt werden sonn
+     */
+     private void setStone(int col, Player currentp) {
+
+        for (int j = Field.length; 0 < j; j--) {
+            if (Field[j-1][col] == '#') {
+                Field[j-1][col] = currentp.getSymbol();
+            }
+
+        }
+    }
 
 
+    /**
+     *     Überprüft ob in der obersten Zeile einer Spalte ein Stein ist, wenn ja wird zurückgegeben, das diese Spalte bereits voll ist.
+      * @param col Welche Spalte gecheckt werden soll
+     * @return Ob die Spalte belegt ist oder nicht.
+     */
+    private boolean checkiffull(int col) {
+        boolean rv = false;
+        if (Field[0][col] != '#') {
+            rv = true;
+        }
+        return rv;
+
+    }
 }
